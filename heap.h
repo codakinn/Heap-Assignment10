5a5d1ed0862bc79b2bbf9932f7dc2d935373a098
codakinn <codakinn@ut.utm.edu>
2017-04-26 19:41:25 -0500

    Second commit: adding class structure for heap

--- heap.h
+++ heap.h
@@ -1,3 +1,36 @@
-#include <iostream>
-#include <string>
+#ifndef NODE_H
+#define NODE_H
 
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <type T>
+class heap {
+ private:
+  vector<T> buffer{
+    
+  }
+  
+ public:
+  heap(){
+    
+  }
+  int size(){
+    
+  }
+  void insert(T value){
+    
+  }
+  void remove_max(){
+    
+  }
+  T max(){
+    
+  }
+  void print(){
+    
+  }
+};
+
+#endif
