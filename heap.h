67a3f7dfb7f29735a8b937d0ec72129c11a83902
codakinn <codakinn@ut.utm.edu>
2017-04-28 12:42:36 -0500

    Third commit: updating heap implementation

--- heap.h
+++ heap.h
@@ -1,5 +1,5 @@
-#ifndef NODE_H
-#define NODE_H
+#ifndef HEAP_H
+#define HEAP_H
 
 #include <vector>
 #include <iostream>
@@ -8,27 +8,85 @@ using namespace std;
 template <type T>
 class heap {
  private:
-  vector<T> buffer{
-    
+  vector<T> buffer;
+  void insertHelper(int current){
+   while(buffer[current] != buffer.front())
+    if(buffer[current] > buffer[(current-1)/2]){
+      T temp = buffer[current];
+      buffer[current] = buffer[(current-1)/2])
+      buffer[(current-1)/2] = temp;
+      insertHelper(value, (current-1)/2);
+    }       
   }
+ 
+ void removeHelper(int current){
+      T *c1 = buffer[2*current +1];
+      T *c2 = buffer[2*(current+1)];
+      T *parent = buffer[current];
+      if(parent < c1 && parent < c2){
+       if(c1 < c2){
+        temp = parent;
+        parent = c2;
+        c2 = temp;
+        removeHelper(2*(current+1));
+       }
+       else if (c2 <= c1){
+        temp = parent;
+        parent = c1;
+        c1 = temp;
+        removeHelper(2*current+1);
+       }
+      }
+      else if(parent < c1 || parent < c2){
+       if(parent < c1){
+        temp = parent;
+        parent = c1;
+        c1 = temp;
+        removeHelper(2*current+1);
+       }
+       else if (parent < c2) {
+        temp = parent;
+        parent = c2;
+        c2 = temp;
+        removeHelper(2*(current+1));
+       }
+      }
+ }
   
  public:
   heap(){
-    
+    buffer.clear();
   }
+ 
   int size(){
-    
+    return buffer.size();
   }
+ 
   void insert(T value){
-    
+   if(buffer.empty())
+     buffer.push_back(value);
+   else{
+     buffer.insert(value);
+     insertHelper(value, size - 1);
+   }
   }
+ 
   void remove_max(){
-    
+     if(!buffer.empty()){
+      int current = 0;
+      T temp = buffer.back();
+      buffer[current] = temp;
+      buffer.pop_back();
+      removeHelper(0);
+     }
   }
   T max(){
-    
+    return buffer.front();
   }
   void print(){
+    vector<T>::const_iterator = i
+    for(i = 0; i < buffer.size(); i++)
+     cout << i << " - " << buffer[i] << endl;
     
   }
 };
