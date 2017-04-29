58437d63b84bcd5723be73b193e9a9173b243abd
Crixorum1992 <nicjreid@ut.utm.edu>
2017-04-28 20:18:32 -0500

    Second Commit: created main file to deal with heap

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
+#include "heap.h"
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    heap<int> h;
+
+    int SOME_NUMBER = 10;
+
+    for(int i=0; i<SOME_NUMBER; i++)
+        h.insert(rand()%100);
+
+    while(h.size() > 0) {
+        cout << h.max() << endl;
+        h.remove_max();
+}
 }
